85cf3e1bc677838d5594db370d0d7e935f6ed648
luisbaquiax <luis.baquiax1995@gmail.com>
2025-09-14 23:18:00 -0600

    add: base inicial del backend

---
+++ SistemasEntregas/src/main/java/org/entregasayd/sistemasentregas/SistemasEntregasApplication.java
@@ -0,0 +1,13 @@
+package org.entregasayd.sistemasentregas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemasEntregasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemasEntregasApplication.class, args);
+    }
+
+}
