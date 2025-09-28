52c86aa4951249f6d860cce57ad60d2cfa3432f4
Rudy Reyes <rudyreyes202031213@cunoc.edu.gt>
2025-09-27 23:09:43 -0600

    add: cancelaciones cliente

--- SistemasEntregas/src/main/java/org/entregasayd/sistemasentregas/controllers/cancelaciones/CancelacionCoordinadorController.java
+++ SistemasEntregas/src/main/java/org/entregasayd/sistemasentregas/controllers/cancelaciones/CancelacionCoordinadorController.java
@@ -8,6 +8,7 @@ import org.entregasayd.sistemasentregas.dto.cancelaciones.*;
 import org.entregasayd.sistemasentregas.services.cancelaciones.*;
 import org.entregasayd.sistemasentregas.models.*;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @RestController
@@ -22,34 +23,57 @@ class CancelacionCoordinadorController {
 
     @GetMapping("/solicitudes")
     public ResponseEntity<?> obtenerSolicitudesCancelacion() {
-        try{
+        try {
             var solicitudes = cancelacionCoordinadorService.obtenerSolicitudesCancelacion();
             return ResponseEntity.ok(solicitudes);
-        }catch(Exception e){
+        } catch (Exception e) {
             return ResponseEntity.status(500).body(Map.of("error", e.getMessage()));
         }
     }
 
     //Aceptar solicitud de cancelacion
     @PostMapping("/solicitud/aceptar")
-    public ResponseEntity<?> aceptarSolicitudCancelacion(@RequestBody AutorizacionCancelacionDTO request){
-        try{
+    public ResponseEntity<?> aceptarSolicitudCancelacion(@RequestBody AutorizacionCancelacionDTO request) {
+        try {
             String resultado = cancelacionCoordinadorService.autorizarSolicitudCancelacion(request);
             return ResponseEntity.ok(Map.of("message", resultado));
-        }catch(Exception e){
+        } catch (Exception e) {
             return ResponseEntity.status(500).body(Map.of("error", e.getMessage()));
         }
     }
 
     @PostMapping("/solicitud/cancelar/{idSolicitud}")
-    public ResponseEntity<?> cancelarSolicitudCancelacion(@PathVariable Long idSolicitud){
-        try{
+    public ResponseEntity<?> cancelarSolicitudCancelacion(@PathVariable Long idSolicitud) {
+        try {
             String resultado = cancelacionCoordinadorService.rechazarSolicitudCancelacion(idSolicitud);
+            return ResponseEntity.ok(Map.of("message", resultado));
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body(Map.of("error", e.getMessage()));
+        }
+    }
+
+    //Cancelaciones del cliente
+    @GetMapping("/cliente/cancelaciones")
+    public ResponseEntity<?> obtenerCancelacionesCliente() {
+        try {
+
+            return ResponseEntity.ok(cancelacionCoordinadorService.obtenerIncidenciasCancelacionClientes());
+        } catch (Exception e) {
+            Map<String, String> resultado = new HashMap<>();
+            resultado.put("error", e.getMessage());
+            return ResponseEntity.status(500).body(resultado);
+        }
+    }
+
+    //Atender cancelacion del cliente
+    @PostMapping("/cliente/cancelacion/atender")
+    public ResponseEntity<?> atenderCancelacionCliente(@RequestBody AceptarCancelacionClienteDTO request){
+        try{
+            String resultado = cancelacionCoordinadorService.aceptarIncidenciaCancelacionCliente(request);
             return ResponseEntity.ok(Map.of("message", resultado));
         }catch(Exception e){
             return ResponseEntity.status(500).body(Map.of("error", e.getMessage()));
         }
     }
-
 
 }
